83894c52bde0a3ec73cb5c7a8286c696539d8285
GPTR <165572453+salihchelebi@users.noreply.github.com>
2026-04-02 10:44:25 +0300

    Fix CI lint formatting and limit Docker build to core workspaces

--- scripts/render-start.js
+++ scripts/render-start.js
@@ -1,32 +1,32 @@
 const path = require('path')
 
 async function main() {
-  const root = path.resolve(__dirname, '..')
+    const root = path.resolve(__dirname, '..')
 
-  const dataSourceMod = require(path.join(root, 'packages/server/dist/DataSource'))
-  const serverMod = require(path.join(root, 'packages/server/dist/index'))
+    const dataSourceMod = require(path.join(root, 'packages/server/dist/DataSource'))
+    const serverMod = require(path.join(root, 'packages/server/dist/index'))
 
-  const DataSource = dataSourceMod.default || dataSourceMod
-  const Server = serverMod.default || serverMod
+    const DataSource = dataSourceMod.default || dataSourceMod
+    const Server = serverMod.default || serverMod
 
-  if (typeof DataSource.init === 'function') {
-    await DataSource.init()
-  }
+    if (typeof DataSource.init === 'function') {
+        await DataSource.init()
+    }
 
-  if (typeof Server.start === 'function') {
-    await Server.start()
-    return
-  }
+    if (typeof Server.start === 'function') {
+        await Server.start()
+        return
+    }
 
-  if (typeof Server === 'function') {
-    await Server()
-    return
-  }
+    if (typeof Server === 'function') {
+        await Server()
+        return
+    }
 
-  throw new Error('Server.start bulunamadi')
+    throw new Error('Server.start bulunamadi')
 }
 
 main().catch((err) => {
-  console.error(err)
-  process.exit(1)
+    console.error(err)
+    process.exit(1)
 })
